b11e4f9948cb2d9b8a87a8ad016cbff759c3d94f
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-05-06 15:26:04 +0200

    Fix records, make compatible with Xcode 8.3+

--- CloudSharingExample/ViewController.swift
+++ CloudSharingExample/ViewController.swift
@@ -10,39 +10,57 @@ import UIKit
 import CloudKit
 
 class ViewController: UIViewController, UICloudSharingControllerDelegate {
-    
+
     @IBAction func didTapCloudSharingButton() {
         
-        // Create image record
-        let fileName = "titanium_mobile"
-        let newRecord = CKRecord(recordType: "ImageRecord", recordID: CKRecordID(recordName: "test"))
-        let image = UIImage(named: fileName)
+        let cloudSharingController = UICloudSharingController(preparationHandler: { controller, preparationCompletionHandler in
+            
+            // Create image record
+            let fileName = "titanium_mobile"
+            let newRecord = CKRecord(recordType: "ImageRecord", recordID: CKRecordID(recordName: "test"))
+            let image = UIImage(named: fileName)
+
+            // Write to filesystem
+            let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+            let imageFilePath = (documentDirectory as NSString).appendingPathComponent(fileName)
+            let myUrl = URL(fileURLWithPath: imageFilePath)
+            
+            do {
+                let data = UIImagePNGRepresentation(image!)
+                try data?.write(to: myUrl, options: .atomicWrite)
+                let asset = CKAsset(fileURL: myUrl)
+                
+                newRecord["image"] = asset
+            } catch {
+                print("Could not write to filesystem")
+            }
+            
+            // Initialize cloud sharing
+            let share: CKShare = CKShare(rootRecord: newRecord)
+            share[CKShareTitleKey] = "\(fileName).png" as CKRecordValue
+            share[CKShareTypeKey] = "com.appcelerator.CloudSharingExample" as CKRecordValue
+            
+            // Prepare records
+            let modifyRecordsOperation = CKModifyRecordsOperation(recordsToSave: [newRecord, share], recordIDsToDelete: nil)
+            
+            modifyRecordsOperation.timeoutIntervalForRequest = 15
+            modifyRecordsOperation.timeoutIntervalForResource = 15
+            
+            modifyRecordsOperation.modifyRecordsCompletionBlock = { records, recordIDs, error in
+                if error != nil {
+                    print("Error: \(String(describing: error?.localizedDescription))")
+                }
+                preparationCompletionHandler(share, CKContainer.default(), error)
+            }
+            
+            OperationQueue().addOperation(modifyRecordsOperation)
+        })
         
-        let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
-        let imageFilePath = (documentDirectory as NSString).appendingPathComponent(fileName)
-        
-        do {
-            try UIImagePNGRepresentation(image!)?.write(to: URL(fileURLWithPath: imageFilePath), options: .atomicWrite)
-        } catch {
-            print("Could not write to filesystem")
-        }
-        
-        let asset = CKAsset(fileURL: NSURL(fileURLWithPath: imageFilePath) as URL)
-        newRecord.setObject(asset, forKey: "ProfilePicture")
-        
-        // Initialize cloud sharing
-        let share: CKShare = CKShare(rootRecord: newRecord)
-        share.setObject("\(fileName).png", forKey: CKShareTitleKey)
-        
-        let container: CKContainer = CKContainer(identifier: "Test")
-        let cloudSharingController: UICloudSharingController = UICloudSharingController(share: share, container: container)
+        // Set sharing delegate
         cloudSharingController.delegate = self
         
         // Set sharing permissions
-        cloudSharingController.availablePermissions = [
-            UICloudSharingPermissionOptions.publicOnly,
-            UICloudSharingPermissionOptions.readWrite
-        ]
+        cloudSharingController.availablePermissions = [.allowPublic, .allowReadOnly]
         
         // Show cloud shareing dialog
         self.present(cloudSharingController, animated: true, completion: nil)
@@ -50,26 +68,21 @@ class ViewController: UIViewController, UICloudSharingControllerDelegate {
     
     // MARK: CloudKit Delegates
     
+    func itemTitle(for csc: UICloudSharingController) -> String? {
+        return "titanium_mobile.png"
+    }
+    
+    @available(iOS 10.0, *)
+    func cloudSharingController(_ csc: UICloudSharingController, failedToSaveShareWithError error: Error) {
+        print("failedToSaveShareWithError: \(error.localizedDescription)")
+    }
+    
     func cloudSharingControllerDidSaveShare(_ csc: UICloudSharingController) {
         print("cloudSharingControllerDidSaveShare")
     }
     
     func cloudSharingControllerDidStopSharing(_ csc: UICloudSharingController) {
         print("cloudSharingControllerDidStopSharing")
-    }
-    
-    func cloudSharingController(_ csc: UICloudSharingController, failedToSaveShareWithError error: NSError) {
-        print("failedToSaveShareWithError: \(error.localizedDescription)")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
